888a80d769f0b1a7785d0221e0f23d91c5391ef7
Sagnik Paul <sagnikpaul2882@gmail.com>
2017-12-03 00:55:39 +0530

    Fixed Speakers page

--- wp-content/plugins/speakers/speakers_shortcode.php
+++ wp-content/plugins/speakers/speakers_shortcode.php
@@ -1,12 +1,18 @@
 <?php
 
 /*
-===============================
-Display all the speakers
-===============================
+==============================================
+Display the speakers in a grid system
+==============================================
 */
 
-function speakers_list_shortcode(){
+function speakers_grid_display($atts, $content = null){
+
+	$atts = shortcode_atts(
+		array(
+			'years' => array('2018', '2017', '2016')
+		), $atts
+	);
 
 	$return_args[] = '<div id="section3">
 			            <div class="animation-element slide slide-left testimonial">
@@ -20,7 +26,14 @@ function speakers_list_shortcode(){
 		'post_type' => 'speakers',
 		'orderby' => 'menu_order',
 		'order' => 'ASC',
-		'post_per_page' => 50
+		'post_per_page' => -1,
+		'tax_query' => array(
+			array(
+				'taxonomy' => 'year',
+				'field' => 'name',
+				'terms' => $atts['years']
+			),
+		),
 	);
 
 	$speakers_list = new WP_Query($args);
@@ -47,15 +60,21 @@ function speakers_list_shortcode(){
 	$args = join($return_args);
 	return $args;
 }
-add_shortcode('speakers-list', 'speakers_list_shortcode');
+add_shortcode('speakers-grid-display', 'speakers_grid_display');
 
 /*
 =============================================================
-Display the modal for long description of the speakers
+Display speakers with pop ups
 =============================================================
 */
 
-function speakers_long_description(){
+function speakers_popup($atts, $content = null){
+
+	$atts = shortcode_atts(
+		array(
+			'years' => array('2018', '2017', '2016')
+		), $atts
+	);
 
 	$return_args[] = '<div id="section3">
 			            <div class="animation-element slide slide-left testimonial">
@@ -69,7 +88,14 @@ function speakers_long_description(){
 		'post_type' => 'speakers',
 		'orderby' => 'menu_order',
 		'order' => 'ASC',
-		'post_per_page' => 50
+		'post_per_page' => -1,
+		'tax_query' => array(
+			array(
+				'taxonomy' => 'year',
+				'field' => 'name',
+				'terms' => $atts['years']
+			),
+		),
 	);
 
 	$speakers_list = new WP_Query($args);
@@ -127,19 +153,25 @@ function speakers_long_description(){
 
 	return $return_args;
 
-
+	
                 
                 
 }
-add_shortcode('speakers-long-descriptions', 'speakers_long_description');
+add_shortcode('speakers-popup', 'speakers_popup');
 
 /*
 ====================================
-Display all the previos speakers
+Display the speakers in a list
 ====================================
 */
 
-function previous_speakers_shortcode(){
+function list_speakers_shortcode($atts, $content = null){
+
+	$atts = shortcode_atts(
+		array(
+			'years' => array('2018', '2017', '2016')
+		), $atts
+	);
 
 	$return_arg[] = '<center><h1>'.get_the_title().'</h1></center><br>';
 
@@ -147,7 +179,14 @@ function previous_speakers_shortcode(){
 		'post_type' => 'speakers',
 		'orderby' => 'menu_order',
 		'order' => 'ASC',
-		'post_per_page' => 50
+		'post_per_page' => -1,
+		'tax_query' => array(
+			array(
+				'taxonomy' => 'year',
+				'field' => 'name',
+				'terms' => $atts['years']
+			),
+		),
 	);
 
 	$speakers_list = new WP_Query($args);
@@ -156,10 +195,7 @@ function previous_speakers_shortcode(){
 	if ($speakers_list->have_posts()):
 		while ($speakers_list->have_posts()): $speakers_list->the_post();
 
-			
-
-			if ($i%2==1){
-				$return_arg[] = '<div class="row" style="padding: 15px;">
+			$return_arg[] = '<div class="row" style="padding: 15px;">
 				                    <div class="col-md-3 col-xs-12" style="padding-top: 15px;">
 				                        <img src="'.get_the_post_thumbnail_url(get_the_ID()).'" class="img-responsive" style="height:250px; width:100%;">
 				                        <br>
@@ -169,19 +205,6 @@ function previous_speakers_shortcode(){
 				                        <p align="justify" style="color: white">'.get_post_meta(get_the_ID(), "long-description", true).'</p>
 				                    </div>
 				                </div>';
-			}else{
-				$return_arg[] = '<div class="row" style="padding: 15px;">
-				                    <div class="col-md-9 text-justify">
-				                        <h2 style="color: white;">'.get_the_title().' </h2>
-				                        <p align="justify" style="color: white">'.get_post_meta(get_the_ID(), "long-description", true).'</p>
-				                    </div>
-				                    <div class="col-md-3 col-xs-12" style="padding-top: 15px;">
-				                        <img src="'.get_the_post_thumbnail_url(get_the_ID()).'" class="img-responsive" style="height:250px; width:100%;">
-				                        <br>
-				                    </div>
-				                    
-				                </div>';
-			}
 
 			$i++;
 
@@ -191,85 +214,5 @@ function previous_speakers_shortcode(){
 	$arg = join($return_arg);
 	return $arg;
 
-			/*<div class="row" style="padding: 15px;">
-                
-                    <div class="col-md-3 col-xs-12">
-                        <img src="'.wp_get_attachment_url(31).'" class="img-responsive" style="height:250px; width:100%;">
-                        <br>
-                    </div>
-                    <div class="col-md-9 text-justify">
-                        <h2 style="color: white;">Lorem Ipsum </h2>
-                        <h5 style="color: white;">
-                            Integer tempus finibus massa, ornare viverra nunc commodo ac. In pellentesque ex eu hendrerit maximus.
-                        </h5>
-
-                        <p align="justify" style="color: white">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dolor tellus,
-                            consequat accumsan accumsan id, tempus sed nisi. Cras a nisi auctor, pharetra dui vel, ornare justo.
-                            Aenean varius elementum efficitur. Vestibulum rhoncus sagittis massa, nec aliquet orci dapibus a.
-                            Aenean augue sem, faucibus sed lorem vel, feugiat pellentesque lacus. Etiam tincidunt, dolor sit amet
-                            interdum convallis, nunc sapien pulvinar erat, venenatis vulputate nisl nisi in nisl. Aenean placerat
-                            felis sed risus pulvinar convallis. Pellentesque euismod diam commodo lorem mattis, in pretium purus
-                            aliquam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
-                            Praesent molestie maximus metus eu volutpat. Nulla condimentum placerat facilisis. Proin non posuere magna.
-                            Praesent pharetra elementum ultricies. Donec ultricies gravida est ac porta.
-                        </p>
-                    </div>
-
-            </div>
-            <div class="row" style="padding: 15px;">
-                
-                    <div class="col-md-3 col-xs-12" style="float: right;">
-                        <img src="'.wp_get_attachment_url(31).'" class="img-responsive" style="height:250px; width:100%;">
-                        <br>
-                    </div>
-                    <div class="col-md-9 text-justify">
-                        <h2 style="color: white;">Lorem Ipsum </h2>
-                        <h5 style="color: white;">
-                            Integer tempus finibus massa, ornare viverra nunc commodo ac. In pellentesque ex eu hendrerit maximus.
-                        </h5>
-
-                        <p align="justify" style="color: white">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dolor tellus,
-                            consequat accumsan accumsan id, tempus sed nisi. Cras a nisi auctor, pharetra dui vel, ornare justo.
-                            Aenean varius elementum efficitur. Vestibulum rhoncus sagittis massa, nec aliquet orci dapibus a.
-                            Aenean augue sem, faucibus sed lorem vel, feugiat pellentesque lacus. Etiam tincidunt, dolor sit amet
-                            interdum convallis, nunc sapien pulvinar erat, venenatis vulputate nisl nisi in nisl. Aenean placerat
-                            felis sed risus pulvinar convallis. Pellentesque euismod diam commodo lorem mattis, in pretium purus
-                            aliquam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
-                            Praesent molestie maximus metus eu volutpat. Nulla condimentum placerat facilisis. Proin non posuere magna.
-                            Praesent pharetra elementum ultricies. Donec ultricies gravida est ac porta.
-                        </p>
-                    </div>
-                
-            </div>
-            <div class="row" style="padding: 15px;">
-                
-                    <div class="col-md-3 col-xs-12">
-                        <img src="'.wp_get_attachment_url(31).'" class="img-responsive" style="height:250px; width:100%;">
-                        <br>
-                    </div>
-                    <div class="col-md-9 text-justify">
-                        <h2 style="color: white;">Lorem Ipsum </h2>
-                        <h5 style="color: white;">
-                            Integer tempus finibus massa, ornare viverra nunc commodo ac. In pellentesque ex eu hendrerit maximus.
-                        </h5>
-    
-                        <p align="justify" style="color: white">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dolor tellus,
-                            consequat accumsan accumsan id, tempus sed nisi. Cras a nisi auctor, pharetra dui vel, ornare justo.
-                            Aenean varius elementum efficitur. Vestibulum rhoncus sagittis massa, nec aliquet orci dapibus a.
-                            Aenean augue sem, faucibus sed lorem vel, feugiat pellentesque lacus. Etiam tincidunt, dolor sit amet
-                            interdum convallis, nunc sapien pulvinar erat, venenatis vulputate nisl nisi in nisl. Aenean placerat
-                            felis sed risus pulvinar convallis. Pellentesque euismod diam commodo lorem mattis, in pretium purus
-                            aliquam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
-                            Praesent molestie maximus metus eu volutpat. Nulla condimentum placerat facilisis. Proin non posuere magna.
-                            Praesent pharetra elementum ultricies. Donec ultricies gravida est ac porta.
-                        </p>
-                    </div>
-                
-            </div>
-        </div>';
-        */
 }
-add_shortcode('previous_speakers', 'previous_speakers_shortcode');
+add_shortcode('speakers-list', 'list_speakers_shortcode');
